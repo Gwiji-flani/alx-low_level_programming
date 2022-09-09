0cb9bc03a108f6de4e788733c25de523b98558b4
benson <bensonmwangi496@gmail.com>
2022-09-08 19:51:06 -0700

    task 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle")
+		return (0);
 }
